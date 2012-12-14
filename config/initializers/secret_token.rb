3ec42ab051f2c5492df2778baab36ddb7c1608de
Dr Blinken <drblinken@gmail.com>
2012-12-14 12:50:24 +0100

    created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdventCalendar::Application.config.secret_token = '69ad8d313fd0e389120104e44efa92b08019a6bbf18318790d284da3ccfd161eb39d2d2a036a9bf180488384b399cf5829ab6e7f3cf0f4eaa36af9bb6768b921'
